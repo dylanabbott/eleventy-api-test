e1c227fc84364a422af5e6a25a86805df4df65fd
dylanabbott <dylanabbott@gmail.com>
2021-12-14 16:15:02 -0800

    Further Cloudinary testing

--- _data/cloudinary.js
+++ _data/cloudinary.js
@@ -2,15 +2,12 @@ const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async () => {
     const response = await Cache(
-      "https://res.cloudinary.com/dylanabbott-com/image/list/portrait.json", 
+      "https://res.cloudinary.com/dylanabbott-com/image/list/test.json", 
       {
           duration: '1d',
           type: 'json'
       }
     );
-    //console.log("response: ", response.json());
-
-    //const data = await response.json();
-    //console.log(data);
+    console.log(response);
     return response;
 };
